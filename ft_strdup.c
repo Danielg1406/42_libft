3270f959d7e1a1c6794fe029d895f7a1f6844d96
Danielg1406 <danielgomeza1406@gmail.com>
2024-04-24 09:33:50 +0200

    creation memcpy, strdup, strlcat, strlcpy, tolower and toupper

---
+++ ft_strdup.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+  char	*str;
+  size_t	len;
+
+  len = ft_strlen(s) + 1;
+  str = (char *)malloc(len);
+  if (str == NULL)
+    return (NULL);
+  ft_memcpy(str, s, len);
+  return (str);
+}
