3cb426bd5899d39ca8a9c5664a5e03414a5cbe36
Daniel Gomez Arango <dgomez-a@c4a9c1.42berlin.de>
2024-04-25 17:53:21 +0200

    correction atoi and substr

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: dgomez-a <dgomez-a@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 15:53:46 by dgomez-a          #+#    #+#             */
-/*   Updated: 2024/04/25 15:53:49 by dgomez-a         ###   ########.fr       */
+/*   Updated: 2024/04/25 17:38:28 by dgomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,27 @@
 
 int	ft_atoi(const char *nptr)
 {
-	int	num;
-	int	sign;
+	int		sign;
+	long	num;
 
 	sign = 1;
 	num = 0;
-	while (*nptr != '\0')
+	while (*nptr == ' ' || (*nptr >= 9 && *nptr <= 13))
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
 	{
-		if (*nptr == '-' || *nptr == '+')
-		{
-			if (*nptr == '-')
-				sign *= -1;
-			nptr++;
-		}
-		if (ft_isdigit(*nptr))
-		{
-			num *= 10 + (*nptr + '0');
-		}
-		else
-			break ;
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		num = num * 10 + *nptr - '0';
+		if (num * sign > 2147483647)
+			return (-1);
+		if (num * sign < -2147483648)
+			return (0);
+		nptr++;
 	}
 	return (num * sign);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -16,12 +16,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 
-	if (!s || len == 0 || start >= ft_strlen(s))
+	if (!s)
 		return (0);
-	sub = (char *)malloc((len + 1) * sizeof(char));
-	if (sub == 0)
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	sub = (char *)malloc(len + 1);
+	if (!sub)
 		return (0);
-	ft_memcpy(sub, &s[start], len);
+	ft_memcpy(sub, s + start, len);
 	sub[len] = '\0';
 	return (sub);
 }
