928a8b215772463e156e32f83ca8450a1f80bbb0
Daniel Gomez Arango <dgomez-a@c4a9c1.42berlin.de>
2024-04-25 19:49:45 +0200

    finish strjoin and start strtrim

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,8 +6,36 @@
 /*   By: dgomez-a <dgomez-a@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 16:32:08 by dgomez-a          #+#    #+#             */
-/*   Updated: 2024/04/25 16:32:10 by dgomez-a         ###   ########.fr       */
+/*   Updated: 2024/04/25 19:12:19 by dgomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strjoin(char const *s1, char const *s2);
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	len_s1;
+	size_t	len_s2;
+	char	*join;
+
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	join = (char *)malloc((len_s1 + len_s2 + 1) * sizeof(char));
+	if (join == 0)
+		return (0);
+	i = 0;
+	while (i <= len_s1)
+	{
+		join[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (i <= len_s2)
+	{
+		join[i + len_s1] = s2[i];
+		i++;
+	}
+	join[len_s1 + len_s2 + 1] = '\0';
+	return (join);
+}
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,8 +6,29 @@
 /*   By: dgomez-a <dgomez-a@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 16:32:23 by dgomez-a          #+#    #+#             */
-/*   Updated: 2024/04/25 16:32:25 by dgomez-a         ###   ########.fr       */
+/*   Updated: 2024/04/25 19:49:02 by dgomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strtrim(char const *s1, char const *set);
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*trim;
+
+	if (!s1 || !set)
+		return (0);
+
+	trim = (char *)malloc((ft_strlen(s1) - ft_strlen(set) + 1) * sizeof(char));
+	if (trim == 0)
+		return (0);
+	ft_strlcpy(trim, (char *)s1, ft_strlen(s1) + 1)
+	return (trim);
+}
+
+#include <stdio.h>
+int main()
+{
+	printf("%s\n", ft_strtrim("helow", "lo"));
+	return 0;
+}
