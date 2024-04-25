bf6df998345bc4c89f62554572f773b2c31d1bf2
Daniel Gomez Arango <dgomez-a@c4a9c1.42berlin.de>
2024-04-25 17:06:19 +0200

    creation substr and adding stdlib in libft.h

--- ft_substr.c
+++ ft_substr.c
@@ -6,10 +6,22 @@
 /*   By: dgomez-a <dgomez-a@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 16:31:47 by dgomez-a          #+#    #+#             */
-/*   Updated: 2024/04/25 16:31:49 by dgomez-a         ###   ########.fr       */
+/*   Updated: 2024/04/25 17:00:33 by dgomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+
+	if (!s || len == 0 || start >= ft_strlen(s))
+		return (0);
+	sub = (char *)malloc((len + 1) * sizeof(char));
+	if (sub == 0)
+		return (0);
+	ft_memcpy(sub, &s[start], len);
+	sub[len] = '\0';
+	return (sub);
+}
